0c9eb1f96adb070ba55b455b80476c28c9b43804
moay <mv@moay.de>
2019-08-07 08:24:21 +0200

    Adds additional tests for path provider

--- tests/PathProvider/PathProviderTest.php
+++ tests/PathProvider/PathProviderTest.php
@@ -19,6 +19,8 @@ class PathProviderTest extends TestCase
         $lowLevelMock = $this->createMock(LowLevel::class);
         $lowLevelMock->method('getUserInfo')
             ->willReturn(null);
+        $lowLevelMock->method('getEnvironmentVariable')
+            ->willReturn(null);
 
         $pathProvider = new PathProvider($lowLevelMock);
         $this->assertEquals($expectedDirectory, $pathProvider->getPath($pathName));
@@ -29,6 +31,30 @@ class PathProviderTest extends TestCase
         return [
             ['LBHOMEDIR', '/opt/loxberry'],
         ];
+    }
+
+    public function testHomeDirCanBeSetViaEnvironmentVariable()
+    {
+        $lowLevelMock = $this->createMock(LowLevel::class);
+        $lowLevelMock->method('getEnvironmentVariable')
+            ->willReturn('TestPath');
+        $lowLevelMock->method('getUserInfo')
+            ->willReturn(null);
+
+        $pathProvider = new PathProvider($lowLevelMock);
+        $this->assertEquals('TestPath', $pathProvider->getPath('LBHOMEDIR'));
+    }
+
+    public function testHomeDirWillBeLoadedFromSystemUser()
+    {
+        $lowLevelMock = $this->createMock(LowLevel::class);
+        $lowLevelMock->method('getEnvironmentVariable')
+            ->willReturn(null);
+        $lowLevelMock->method('getUserInfo')
+            ->willReturn(['uid' => ['dir' => 'TestPath2']]);
+
+        $pathProvider = new PathProvider($lowLevelMock);
+        $this->assertEquals('TestPath2', $pathProvider->getPath('LBHOMEDIR'));
     }
 
     // Todo: Test env variable and user info mapping for LBHOMEDIR as well as logging
