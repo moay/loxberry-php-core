56baa1107aae21fdf9456b2ea3b66d9659f1ad9d
moay <mv@moay.de>
2019-08-31 15:32:29 +0200

    Adds translation file parser

---
+++ src/System/Localization/LanguageFileParser.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace LoxBerry\System\Localization;
+
+/**
+ * Class LanguageFileParser.
+ */
+class LanguageFileParser
+{
+    /** @var string */
+    private $fileName;
+
+    /** @var array */
+    private $parsedTranslations = [];
+
+    /**
+     * LanguageFileParser constructor.
+     *
+     * @param string $fileName
+     */
+    public function __construct(string $fileName)
+    {
+        $this->fileName = $fileName;
+        $this->parseTranslations();
+    }
+
+    /**
+     * @param string      $section
+     * @param string|null $key
+     *
+     * @return string|null
+     */
+    public function getTranslated(string $section, ?string $key = null): ?string
+    {
+        if (null === $key) {
+            $key = explode('.', $section)[1] ?? '';
+            $section = explode('.', $section)[0] ?? '';
+        }
+
+        if (array_key_exists($section, $this->parsedTranslations)
+            && array_key_exists($key, $this->parsedTranslations[$section])) {
+            return $this->parsedTranslations[$section][$key];
+        }
+
+        return '';
+    }
+
+    private function parseTranslations()
+    {
+        if (!file_exists($this->fileName) || !is_readable($this->fileName)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Non existing translation file %s requested.',
+                $this->fileName
+            ));
+        }
+
+        $contents = file($this->fileName, FILE_SKIP_EMPTY_LINES);
+        if (!$contents) {
+            throw new \InvalidArgumentException(sprintf(
+                'Malformed translation file %s requested. Unable to parse.',
+                $this->fileName
+            ));
+        }
+
+        $filteredContents = $this->removeCommentLines($contents);
+        $section = '';
+        foreach ($filteredContents as $line) {
+            if (preg_match('/\[(\S+)\]/', $line, $matches)) {
+                $section = $matches[1];
+                continue;
+            }
+            if (preg_match('/(\S+)="(.*)"/', $line, $matches)) {
+                $this->parsedTranslations[$section][$matches[1]] = trim($matches[2]);
+                continue;
+            }
+            if (preg_match('/(\S+)=(.*)/', $line, $matches)) {
+                $this->parsedTranslations[$section][$matches[1]] = trim($matches[2]);
+            }
+        }
+    }
+
+    /**
+     * @param array $contents
+     *
+     * @return array
+     */
+    private function removeCommentLines(array $contents): array
+    {
+        return array_filter($contents, function ($line) {
+            return !in_array(substr($line, 0, 1), ['#', '/', ';']);
+        });
+    }
+}
---
+++ tests/Localization/LanguageFileParserTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace LoxBerry\Tests\Localization;
+
+use LoxBerry\System\Localization\LanguageFileParser;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class LanguageFileParserTest.
+ */
+class LanguageFileParserTest extends TestCase
+{
+    const TEST_FILE = __DIR__.'/resources/language_en.ini';
+
+    /**
+     * @dataProvider validTranslations
+     */
+    public function testLoadsAndTranslatesSectionAndKey($section, $key, $expectedTranslation)
+    {
+        $parser = new LanguageFileParser(self::TEST_FILE);
+        $this->assertSame($expectedTranslation, $parser->getTranslated($section, $key));
+    }
+
+    /**
+     * @dataProvider validTranslations
+     */
+    public function testLoadsAndTranslatesSectionKeyCombined($section, $key, $expectedTranslation)
+    {
+        $parser = new LanguageFileParser(self::TEST_FILE);
+        $this->assertSame($expectedTranslation, $parser->getTranslated($section.'.'.$key));
+    }
+
+    public function testReturnsEmptyStringIfUnknownSectionAndOrKey()
+    {
+        $parser = new LanguageFileParser(self::TEST_FILE);
+        $this->assertSame('', $parser->getTranslated('SECUREPIN', 'LABEL_NEW_SECUREPIN'));
+        $this->assertSame('', $parser->getTranslated('SECUREPIN.LABEL_NEW_SECUREPIN'));
+    }
+
+    public function testThrowsExceptionIfFileDoesNotExist()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $parser = new LanguageFileParser(self::TEST_FILE.'foobar');
+    }
+
+    public function validTranslations()
+    {
+        return [
+          ['ADMIN', 'WIDGETLABEL', 'Administrator'],
+          ['ADMIN', 'LABEL_NEW_SECUREPIN', 'New SecurePIN:'],
+          ['SECUREPIN', 'SUCCESS', 'You have entered the correct SecurePIN.'],
+        ];
+    }
+}
